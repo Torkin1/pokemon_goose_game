9f0941b5eeb43edc87d5bfd64bb022fc5296988a
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-11-09 00:53:34 +0100

    #20

--- PokemonGooseGame/app/src/main/java/it/walle/pokemongoosegame/game/NoGameSetupException.java
+++
@@ -1,8 +0,0 @@
-package it.walle.pokemongoosegame.game;
-
-
-public class NoGameSetupException extends Throwable {
-    public NoGameSetupException() {
-        super("No game setup. Call GameController.setupGame to setup a new Game instance");
-    }
-}
