d87af07be87d91889c21aa553d79cef3e1f7b857
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-12-22 23:33:17 +0100

    #105, forgot to delete some files

--- PokemonGooseGame/app/src/main/java/it/walle/pokemongoosegame/utils/DrawableNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package it.walle.pokemongoosegame.utils;
-
-public class DrawableNotFoundException extends Throwable {
-    public DrawableNotFoundException(String name) {
-        super("drawable not found with name " + name);
-    }
-}
