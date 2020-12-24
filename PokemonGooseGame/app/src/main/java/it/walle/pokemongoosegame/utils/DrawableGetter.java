8476782a36d2cf657baaaad8368e085001d46ef3
Daniele La Prova <32265040+Torkin1@users.noreply.github.com>
2020-12-24 18:14:17 +0100

    #112

---
+++ PokemonGooseGame/app/src/main/java/it/walle/pokemongoosegame/utils/DrawableGetter.java
@@ -0,0 +1,48 @@
+package it.walle.pokemongoosegame.utils;
+
+import android.util.Log;
+
+import java.lang.reflect.Field;
+
+
+import it.walle.pokemongoosegame.R;
+import it.walle.pokemongoosegame.entity.board.cell.Cell;
+
+public class DrawableGetter {
+
+    private static DrawableGetter ref;
+    public static DrawableGetter getReference(){
+        if (ref == null){
+            ref = new DrawableGetter();
+        }
+        return ref;
+    }
+
+    private final static String TAG = DrawableGetter.class.getSimpleName();
+
+    private int getDrawableId(String drawableName) throws DrawableNotFoundException {
+        // Retrieves drawable id corresponding to the provided name
+        Field[] allDrawableFields = R.drawable.class.getFields();
+        for (Field f : allDrawableFields){
+            if (f.getName().compareTo(drawableName) == 0){
+                try {
+                    return (Integer) f.get(null);
+                } catch (IllegalAccessException e) {
+                    Log.e(TAG, e.getMessage(), e);
+                }
+            }
+        }
+        throw new DrawableNotFoundException(drawableName);
+    }
+
+    public Integer getTypeDrawableId(String typeName) throws DrawableNotFoundException {
+
+        return getDrawableId(typeName);
+    }
+
+    public int getCellBgDrawableId(Class<? extends Cell> cellClass) throws DrawableNotFoundException {
+        final String DRAWABLE_BG_BASE_NAME = "cell_bg_";
+        String drawableBgName = DRAWABLE_BG_BASE_NAME + cellClass.getSimpleName().toLowerCase();
+        return getDrawableId(drawableBgName);
+    }
+}
