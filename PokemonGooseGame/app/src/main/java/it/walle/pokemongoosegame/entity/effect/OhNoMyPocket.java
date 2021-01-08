a3f45a19b3ea39ed93d4fdf5d31bd2e42c1739dc
mihaiJianu <45464636+mihaiJianu@users.noreply.github.com>
2021-01-08 23:55:26 +0100

    #198

--- PokemonGooseGame/app/src/main/java/it/walle/pokemongoosegame/entity/effect/OhNoMyPocket.java
+++ PokemonGooseGame/app/src/main/java/it/walle/pokemongoosegame/entity/effect/OhNoMyPocket.java
@@ -7,19 +7,19 @@ import it.walle.pokemongoosegame.entity.Player;
 import it.walle.pokemongoosegame.game.CoreController;
 
 public class OhNoMyPocket extends YellowEffect{
-    int ADDED_COINS = 20;
-    int MALUS_POSITION = -6;
+    int LOST_COINS = 20;
+    int MALUS_POSITION = 6;
     @Override
     public void doEffect(InvocationContext invocationContext) {
         super.setEffect_image_dialogID(R.drawable.oh_no);
-        super.setDescription(invocationContext.getContext().getString(R.string.oh_no_my_pocket_yellow_effect_description));
         super.setTitle(invocationContext.getContext().getString(R.string.oh_no_my_pocket_yellow_effect_title));
+        super.setDescription(invocationContext.getContext().getString(R.string.oh_no_my_pocket_yellow_effect_description));
         super.generalDialog(invocationContext).show();
 
         Player player = CoreController.getReference().getPlayerByUsername(invocationContext.getTriggerUsername());
-        player.setMoney(player.getMoney() + ADDED_COINS);
+        player.setMoney(player.getMoney() - LOST_COINS);
 
-        player.setCurrentPosition(player.getCurrentPosition() + MALUS_POSITION);
+        player.setCurrentPosition(player.getCurrentPosition() - MALUS_POSITION);
         generalDialog(invocationContext);
     }
 }
